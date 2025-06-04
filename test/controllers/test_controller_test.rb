620ca26528b763284590450af6e58f7c04be20dd
Tran Vu Lam <lam261204@gmail.com>
2025-06-04 15:27:05 +0700

    uncomment route get test/hello

--- test/controllers/test_controller_test.rb
+++ test/controllers/test_controller_test.rb
@@ -1,8 +1,15 @@
 require "test_helper"
 
 class TestControllerTest < ActionDispatch::IntegrationTest
-  test "should get hello" do
+  test "should get hello via root" do
+    get root_url
+    assert_response :success
+    assert_match "hello Lam", response.body
+  end
+
+  test "should get hello via test/hello" do
     get test_hello_url
     assert_response :success
+    assert_match "hello Lam", response.body
   end
 end
